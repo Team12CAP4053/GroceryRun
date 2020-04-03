2c974a803a5efce3fce067e67d9a9bffcece2fc5
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 14:58:49 -0400

    Started using prefabs on project, readded rotato potato on boxes

--- GroceryRunShoppingKarts/Assets/Scripts/MysteryBoxLogic.cs
+++ GroceryRunShoppingKarts/Assets/Scripts/MysteryBoxLogic.cs
@@ -6,17 +6,16 @@ public class MysteryBoxLogic : MonoBehaviour
 {
 
     //adjust this to change speed
-    public float speed = 3f;
-    //adjust this to change how high it goes
-    public float height = 0.5f;
+    public float speed = 0.5f;
     // Update is called once per frame
+    float rotation = 0;
     void Update()
     {
-        //get the objects current position and put it in a variable so we can access it later with less code
-        Vector3 pos = transform.position;
-        //calculate what the new Y position will be
-        float newY = Mathf.Sin(Time.time * speed) * height;
-        //set the object's Y to the new calculated Y
-        transform.position = new Vector3(pos.x, newY + pos.y, pos.z);
+
+        rotation += speed;
+        Quaternion target = Quaternion.Euler(0, rotation, 0);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 0.5f);
     }
 }
