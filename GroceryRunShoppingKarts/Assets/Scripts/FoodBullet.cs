5b05a70a5e29294e691a358625c5b1c04edde4d7
Jake <jrbelga@gmail.com>
2020-04-06 01:53:02 -0400

    Made changes to Player model and added shooting mechanics

---
+++ GroceryRunShoppingKarts/Assets/Scripts/FoodBullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBullet : MonoBehaviour
+{
+    public float speed = 100.0f;
+    public float lifetime = 3.0f;
+
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = lifetime;
+    }
+
+    void Update()
+    {
+        // Bullet moves forward!
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        // Destroy bullet after timer == 0
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject otherObj = collision.gameObject;
+        Debug.Log("Collided with: " + otherObj);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject otherObj = collider.gameObject;
+        Debug.Log("Triggered with: " + otherObj);
+    }
+}
---
+++ GroceryRunShoppingKarts/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject watermelon;
+    public GameObject chicken;
+    public GameObject apple;
+    public GameObject milkjug;
+    public GameObject forwardBox;
+    public SphereCollider sphereBulletCollider;
+    public BoxCollider boxBulletCollider;
+
+
+    // Change to actual member of karitem later
+    public int itemVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemVal = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("f"))
+        {
+            // Apple
+            if (itemVal == 0)
+            {
+                GameObject bulletObject = Instantiate(apple) as GameObject;
+                bulletObject.transform.position = transform.position + transform.forward * 5 + transform.up * 2;
+                bulletObject.transform.forward = forwardBox.transform.forward;
+                bulletObject.transform.localScale = new Vector3(10.0f, 10.0f, 10.0f);
+                bulletObject.AddComponent<FoodBullet>();
+                bulletObject.AddComponent<SphereCollider>();
+                bulletObject.AddComponent<Rigidbody>();
+                bulletObject.GetComponent<Rigidbody>().useGravity = false;
+
+
+                sphereBulletCollider = bulletObject.GetComponent<SphereCollider>();
+                sphereBulletCollider.radius = 0.07f;
+                sphereBulletCollider.center = new Vector3(0.0f, 0.05f, 0.0f);
+            }
+            // Chicken
+            else if (itemVal == 1)
+            {
+                GameObject bulletObject = Instantiate(chicken) as GameObject;
+                bulletObject.transform.position = transform.position + transform.forward * 5 + transform.up * 2;
+                bulletObject.transform.forward = forwardBox.transform.forward;
+                bulletObject.transform.localScale = new Vector3(3.0f, 3.0f, 3.0f);
+                bulletObject.AddComponent<FoodBullet>();
+                bulletObject.AddComponent<BoxCollider>();
+                bulletObject.AddComponent<Rigidbody>();
+                bulletObject.GetComponent<Rigidbody>().useGravity = false;
+
+                boxBulletCollider = bulletObject.GetComponent<BoxCollider>();
+            }
+            // Milk Jug
+            else if (itemVal == 2)
+            {
+                GameObject bulletObject = Instantiate(milkjug) as GameObject;
+                bulletObject.transform.position = transform.position + transform.forward * 5 + transform.up * 2;
+                bulletObject.transform.forward = forwardBox.transform.forward;
+                bulletObject.transform.localScale = new Vector3(6.0f, 6.0f, 6.0f);
+                bulletObject.AddComponent<FoodBullet>();
+                bulletObject.AddComponent<BoxCollider>();
+                bulletObject.AddComponent<Rigidbody>();
+                bulletObject.GetComponent<Rigidbody>().useGravity = false;
+
+                boxBulletCollider = bulletObject.GetComponent<BoxCollider>();
+            }
+            // Watermelon
+            else if (itemVal == 3)
+            {
+                GameObject bulletObject = Instantiate(watermelon) as GameObject;
+                bulletObject.transform.position = transform.position + transform.forward * 5 + transform.up * 2;
+                bulletObject.transform.forward = forwardBox.transform.forward;
+                bulletObject.transform.localScale = new Vector3(7.0f, 7.0f, 7.0f);
+                bulletObject.AddComponent<FoodBullet>();
+                bulletObject.AddComponent<SphereCollider>();
+                bulletObject.AddComponent<Rigidbody>();
+                bulletObject.GetComponent<Rigidbody>().useGravity = false;
+
+                sphereBulletCollider = bulletObject.GetComponent<SphereCollider>();
+                sphereBulletCollider.radius = 0.2f;
+                sphereBulletCollider.center = new Vector3(0.0f, 0.15f, 0.0f);
+            }
+        }
+    }
+}
