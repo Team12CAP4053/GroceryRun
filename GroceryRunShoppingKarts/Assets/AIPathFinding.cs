6c096f3e948bc81da47f6f567e845c317cf2c0e9
Matt Bautista <nayoshi12@gmail.com>
2020-04-06 20:27:16 -0400

    QOL stuff

---
+++ GroceryRunShoppingKarts/Assets/AIPathFinding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class AIPathFinding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NextCheckpoint checkpoint;
+    GameObject Leonardo;
+    void Start()
+    {
+        Leonardo = GameObject.FindWithTag("Leonardo");
+        Leonardo.GetComponent<CarAIControl>().SetTarget(checkpoint.GetComponent<Transform>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.tag == "Checkpoint")
+        {
+            checkpoint = collider.gameObject.GetComponent<NextCheckpoint>().next;
+            Debug.Log(checkpoint.name);
+            Leonardo.GetComponent<CarAIControl>().SetTarget(checkpoint.GetComponent<Transform>());
+        }
+    }
+}
